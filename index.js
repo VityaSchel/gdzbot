d9b61f8f0c17ffe1d5df0367667ebd120c4c3d16
Viktor <vityaschel@utidteam.com>
2021-09-19 20:03:42 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 40404
+
+app.post('/', (req, res) => {
+  console.log(req) //{ "type": "confirmation", "group_id": 188903082 }
+  res.send('820bbe48')
+})
+
+app.listen(port, () => {
+  console.log(`GDZ bot app listening at http://localhost:${port}`)
+})
