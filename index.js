5f34378c3b89176cf0781897686ea32605b61a7e
Viktor <vityaschel@utidteam.com>
2021-09-19 21:16:47 +0400

    fetch homework

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from 'express'
 const app = express()
 const port = 40404
+import fetch from 'node-fetch'
+import { parse } from 'node-html-parser'
 
 app.use(express.json())
+
+const getPictures = async numbers => {
+  const apiURL = `https://gdz.ru/class-10/algebra/nikolskij-potapov/${numbers[0]}-item-${numbers[1]}/`
+  let gdz = await fetch(apiURL)
+  let gdzPage = await gdz.text()
+  const root = parse(gdzPage)
+  const solution = root.querySelector('.with-overtask > img').src
+  console.log(numbers.join('.'), solution)
+}
 
 app.post('/', (req, res) => {
   if(req.body.secret === 'satana_mogila_kladbische_govno_hyila'){
@@ -14,9 +25,9 @@ app.post('/', (req, res) => {
       let homework = numbers.map(n => {
         let letters = n.match(/[а-яА-Я]/g)
         let number = n.match(/[1-3]\.[1-9]{0,3}/)[0]
-        return [number, letters]
+        return [number.split('.'), letters]
       })
-      console.log(homework)
+      homework.forEach(hw => getPictures(hw[0]))
     }
   }
   res.send('ok')
